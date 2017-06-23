3cdafa714ce487d1de87f3c4da1d4d62ed7cf8e2
James Morris <actuallyjamez@gmail.com>
2017-06-23 22:36:53 +0100

    New Setup Wizard and config file

--- macsaver.py
+++ macsaver.py
@@ -1,26 +1,45 @@
 from flask import *
 from functools import *
 from flask_sqlalchemy import SQLAlchemy
-from config import *
+
+import os
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('config.ini')
+config.add_section('database')
+config.add_section('user')
+
+config.set('database', 'id', '')
+config.set('database', 'user', '')
+config.set('database', 'pass', '')
+config.set('database', 'host', '')
+
 app = Flask(__name__)
 app.secret_key = 'SuperSecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://' + \
-                                        DATABASE_USER + \
-                                        ':' + \
-                                        DATABASE_PASSWORD + \
-                                        '@' + \
-                                        DATABASE_ADDRESS + \
-                                        '/' + \
-                                        DATABASE_NAME
+try:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://' + \
+                                            config.get('database', 'user') + \
+                                            ':' + \
+                                            config.get('database', 'pass') + \
+                                            '@' + \
+                                            config.get('database', 'host') + \
+                                            '/' + \
+                                            config.get('database', 'id')
+except:
+    pass
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+installed = dir_path + '/config.ini'
+
 
 class Scanner(db.Model):
-    __tablename__ = DATABASE_TABLE
+    __tablename__ = 'scanner'
     id = db.Column('id', db.INT, primary_key=True)
-    mac = db.Column('mac', db.Unicode)
-    name = db.Column('name', db.Unicode)
+    mac = db.Column('mac', db.Unicode(20))
+    name = db.Column('name', db.Unicode(80))
 
     def __init__(self, mac, name):
         self.mac = mac
@@ -41,10 +60,29 @@ def login_required(f):
     return wrap
 
 
+def install_check(f):
+    # noinspection PyGlobalUndefined
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if os.path.exists(installed):
+            return f(*args, **kwargs)
+        else:
+            global not_installed
+            not_installed = True
+
+            return redirect(url_for('setup'))
+    return wrap
+
+
 @app.route('/')
+@install_check
 @login_required
 def home():
-    data = Scanner.query.all()
+    try:
+        data = Scanner.query.all()
+    except:
+        return render_template('index.html')
+
     return render_template('index.html', data=data)
 
 
@@ -53,7 +91,7 @@ def login():
     if request.method == 'POST':
         username = request.form['user']
         password = request.form['pass']
-        if username == USERNAME and password == PASSWORD:
+        if username == config.get('user', 'username') and password == config.get('user', 'password'):
             session['logged_in'] = True
             return redirect(url_for('home'))
         else:
@@ -75,8 +113,8 @@ def new():
     return render_template('new.html')
 
 
-@login_required
 @app.route('/_action_remove', methods=['POST', 'GET'])
+@login_required
 def action_remove():
     entry = request.form['id']
     Scanner.query.filter_by(id=entry).delete()
@@ -91,5 +129,45 @@ def logout():
     return redirect(url_for('login'))
 
 
+@app.route('/test')
+def test():
+    return 'test'
+
+
+@app.route('/setup', methods=['GET', 'POST'])
+def setup():
+    if os.path.exists(installed):
+        return abort(404)
+    if request.method == 'POST':
+        database_id = request.form['database_id']
+        database_user = request.form['database_user']
+        database_pass = request.form['database_pass']
+        database_host = request.form['database_host']
+        user_user = request.form['user_user']
+        user_pass = request.form['user_pass']
+        print(database_id)
+        print(database_user)
+        print(database_pass)
+        print(database_host)
+        config.set('database', 'id', database_id)
+        config.set('database', 'user', database_user)
+        config.set('database', 'pass', database_pass)
+        config.set('database', 'host', database_host)
+        config.set('user', 'username', user_user)
+        config.set('user', 'password', user_pass)
+        with open(os.path.dirname(os.path.realpath(__file__)) + '/config.ini', 'w') as f:
+            config.write(f)
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://' + \
+                                                config.get('database', 'user') + \
+                                                ':' + \
+                                                config.get('database', 'pass') + \
+                                                '@' + \
+                                                config.get('database', 'host') + \
+                                                '/' + \
+                                                config.get('database', 'id')
+        db.create_all()
+        return redirect(url_for('home'))
+    return render_template('setup.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, threaded=True)
